bcf71c59fafbb5aa80c03befd43ea38ef6182482
surya.singh <suryapsingh@L-156179489-SURYAPRAKASHSINGH.local>
2021-03-08 19:37:14 +0000

    Feat:(Component Main)

---
+++ src/component/Main/Main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Main.scss";
+
+/**
+ * Main component.
+ */
+const Main: React.FC = () => {
+  
+  return (
+    <div data-testid="main" className="bt--container-main">
+      Main
+    </div>
+  );
+};
+
+export default Main;
